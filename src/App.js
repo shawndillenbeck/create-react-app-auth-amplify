4ec6f5e0d9e8439302170df126747ab01c239e6d
shawndillenbeck <63478293+shawndillenbeck@users.noreply.github.com>
2020-04-15 06:56:43 -0500

    Testing Federated login

--- src/App.js
+++ src/App.js
@@ -1,32 +1,40 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { withAuthenticator } from 'aws-amplify-react'
-import Amplify, { Auth } from 'aws-amplify';
-import aws_exports from './aws-exports';
-Amplify.configure(aws_exports);
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import awsconfig from './aws_exports';
+Amplify.configure(awsconfig);
+
 
 class App extends Component {
+  state = { user: null, customState: null };
+
+  componentDidMount() {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+          this.setState({ user: data });
+          break;
+        case "signOut":
+          this.setState({ user: null });
+          break;
+        case "customOAuthState":
+          this.setState({ customState: data });
+      }
+    });
+
+    Auth.currentAuthenticatedUser()
+      .then(user => this.setState({ user }))
+      .catch(() => console.log("Not signed in"));
+  }
+
   render() {
+    const { user } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={() => Auth.federatedSignIn({provider: 'Facebook'})}>Open Facebook</button>
+        <button onClick={() => Auth.federatedSignIn({provider: 'Google'})}>Open Google</button>
+        <button onClick={() => Auth.federatedSignIn()}>Open Hosted UI</button>
+        <button onClick={() => Auth.signOut()}>Sign Out {user.getUsername()}</button>
       </div>
     );
   }
 }
-
-export default withAuthenticator(App, true);
