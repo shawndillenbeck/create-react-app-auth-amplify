f2241b5cf4bdc91a05142dea3af02d72892a6b83
shawndillenbeck <63478293+shawndillenbeck@users.noreply.github.com>
2020-04-15 11:00:09 -0500

    Trying out OAuth

--- src/App.js
+++ src/App.js
@@ -7,23 +7,36 @@ import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
 class App extends Component {
+  state = { user: null, customState: null };
+
+  componentDidMount() {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      switch (event) {
+        case "signIn":
+          this.setState({ user: data });
+          break;
+        case "signOut":
+          this.setState({ user: null });
+          break;
+        case "customOAuthState":
+          this.setState({ customState: data });
+      }
+    });
+
+    Auth.currentAuthenticatedUser()
+      .then(user => this.setState({ user }))
+      .catch(() => console.log("Not signed in"));
+  }
+
   render() {
+    const { user } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <button onClick={() => Auth.federatedSignIn({provider: 'Facebook'})}>Open Facebook</button>
+        <button onClick={() => Auth.federatedSignIn({provider: 'Google'})}>Open Google</button>
+        <button onClick={() => Auth.federatedSignIn()}>Open Hosted UI</button>
+        <button onClick={() => Auth.signOut()}>Sign Out {user.getUsername()}</button>
       </div>
     );
   }
