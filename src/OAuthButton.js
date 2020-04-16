772011e5c6ee0dfca5fd6f17fd51439354a07cc7
shawndillenbeck <63478293+shawndillenbeck@users.noreply.github.com>
2020-04-15 23:27:03 -0500

    Update OAuthButton.js

--- src/OAuthButton.js
+++ src/OAuthButton.js
@@ -2,9 +2,14 @@ import { withOAuth } from 'aws-amplify-react';
 import React, { Component } from 'react';
 
 class OAuthButton extends Component {
+  handleClick() {
+    // do something meaningful, Promises, if/else, whatever, and then
+    window.location.assign('https://pr-landing-page.auth.us-west-2.amazoncognito.com/oauth2/authorize?identity_provider=Okta&redirect_uri=https://master.d33pteq2oqztkg.amplifyapp.com/&response_type=TOKEN&client_id=4mbferift1eu845umvfe5el88q&state=rwi1RTzgj7WcsCvYgDSvaUdOErHxhEHP&scope=email%20openid');
+  }
+  
   render() {
     return (
-      <button onClick={this.props.OAuthSignIn}>
+      <button onClick={this.handleClick}>
         Sign in with AWS
       </button>
     )
