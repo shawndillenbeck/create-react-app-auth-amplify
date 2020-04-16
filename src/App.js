7bead4761d3a1a2cdfd3e1e9c594204097cd2fb7
shawndillenbeck <63478293+shawndillenbeck@users.noreply.github.com>
2020-04-15 23:04:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const oauth = {
   scope: ['email',  'openid'],
   redirectSignIn: 'https://master.d33pteq2oqztkg.amplifyapp.com/',
   
-  responseType: 'code' // or 'token', note that REFRESH token will only be generated when the responseType is code
+  responseType: 'token' // or 'token', note that REFRESH token will only be generated when the responseType is code
 };
 
 Amplify.configure(awsconfig);
